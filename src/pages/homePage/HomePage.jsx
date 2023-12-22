48e8cb7516aeab49bb79dd8a7a87c09a7d1544ab
MohammedSARHANE1 <mohamedsarhane0@gmail.com>
2023-12-22 13:22:19 +0100

    creacte HomePage

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex,Box,Text,Image, Heading } from '@chakra-ui/react'
+import {Button,Box,Text,Image, Heading } from '@chakra-ui/react'
 import Styles from './HomePage.module.css'
 import NavBar from './component/NavBar'
 function HomePage() {
@@ -7,16 +7,18 @@ function HomePage() {
     <div className={Styles.nav}>
      <NavBar/>
      <Box   display="grid" alignItems="center" justifyItems="center">
-       <Heading  >you Travel World</Heading>
-       <Heading  >MorrocanWise keeps track of your</Heading>
-       <Heading  >Advantures</Heading>
-     
+       <Heading as='h2' size='lg' >you Travel World</Heading>
+       <Heading as='h2' size='lg' >MorrocanWise keeps track of your</Heading>
+       <Heading as='h2' size='lg'>Advantures</Heading>
      </Box>
-     <Box>
-     <Heading   as='h1' size='m' >
-     specialised in React
-  </Heading>
-     </Box>
+    <Box className={Styles.text} display="grid" alignItems="center" justifyItems="center">
+    <Text display="grid" alignItems="center" justifyItems="center">
+    <span> A word map that tracks your footsteps into every city you can</span> 
+     <span>thinks of . never forgetyour wonderful experience, and show your friend how you </span> 
+     <span>have wanderred the world. </span>
+    </Text>
+    <Button backgroundColor=""> START TRACKING NOW</Button>
+    </Box>
     </div>
   )
 }
